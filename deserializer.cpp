5560eb8a1c2a24a9a2bda4c53e4d19c4f526c50b
samvishnevskiy <sam.vishnevskiy@gmail.com>
2025-12-21 13:46:33 -0600

    Update deserializer.cpp to use streams

--- deserializer.cpp
+++ deserializer.cpp
@@ -1,22 +1,23 @@
 // This is based on a tutorial from https://docs.amd.com/r/en-US/ug1399-vitis-hls/HLS-Stream-Library
 
-#include <cstdint>
-
+#include <ap_axi_sdata.h>
+#include <hls_stream.h>
 
 extern "C" {
-    void deserialize(uint8_t* stream_in, uint8_t* stream_out, unsigned int stream_length){
-        #pragma HLS INTERFACE m_axi  port=stream_in   offset=slave bundle=gmem0      
-        #pragma HLS INTERFACE m_axi  port=stream_out  offset=slave bundle=gmem1
-        #pragma HLS INTERFACE s_axilite port=stream_in
-        #pragma HLS INTERFACE s_axilite port=stream_out
+    void deserialize(
+        hls::stream<ap_axis<8,0,0,0>>& stream_in,  // AXI-Stream interface
+        hls::stream<ap_axis<8,0,0,0>>& stream_out,
+        unsigned int stream_length)
+    {
+        #pragma HLS INTERFACE axis port=stream_in    
+        #pragma HLS INTERFACE axis port=stream_out   
         #pragma HLS INTERFACE s_axilite port=stream_length
         #pragma HLS INTERFACE s_axilite port=return
-
+        
         for(unsigned int i = 0; i < stream_length; i++) {
-            #pragma HLS PIPELINE II=1       //this pragma pipelines the loop so one loop iteration corresponds to one clock cycle
-            stream_out[i] = stream_in[i]; 
+            #pragma HLS PIPELINE II=1
+            ap_axis<8,0,0,0> temp = stream_in.read();
+            stream_out.write(temp);
         }
     }
 }
-
-
